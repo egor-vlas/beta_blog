39ef458bd6da891e1e6b0c712411993f7713a3f3
Iegor <vlasenkoiegor2021@gmail.com>
2021-05-11 08:43:56 +1000

    errors messaging system implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,17 @@ class ArticlesController < ApplicationController
 
 
 	def new
-
+		@article = Article.new
 	end
 
 	def create
 		@article = Article.new(params.require(:article).permit(:title, :description))
-		@article.save
-		redirect_to @article
+		if @article.save
+		   flash[:notce] = "Article was created successfully."
+		  redirect_to @article
+		else
+		  render 'new'
+		end
 	end
 
 
