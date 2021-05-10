c7c02c282e2e938c5e8ca96427dd7ad6736b1472
Iegor <vlasenkoiegor2021@gmail.com>
2021-05-10 16:21:43 +1000

    feature of saving posts to database using browser added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
+
+
+	def new
+
+	end
+
+	def create
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		@article.save
+		redirect_to @article
+	end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 
 end
 
