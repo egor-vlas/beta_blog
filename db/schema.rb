61c9ffc7e9eb085161216b10ce34c21a6174f682
Iegor <vlasenkoiegor2021@gmail.com>
2021-05-10 13:44:38 +1000

    show page added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_024930) do
+ActiveRecord::Schema.define(version: 2021_05_09_035106) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
