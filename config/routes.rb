d30a48abf3592b3782c773aa9f9d4965013e9cd3
Iegor <vlasenkoiegor2021@gmail.com>
2021-05-12 14:07:33 +1000

    destroy action added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
 
 end
 
