958e8b7b1762b2b41516a1f87553e5913a7d7591
Iegor <vlasenkoiegor2021@gmail.com>
2021-05-28 13:10:35 +1000

    sign up added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 
 end
 
