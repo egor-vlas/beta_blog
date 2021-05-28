26a9d94e5ab4b43967561d55e8f3657f773a2e6b
Iegor <vlasenkoiegor2021@gmail.com>
2021-05-28 16:48:12 +1000

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = "Welcome to Beta Blog #{@user.username} you successfully signed up"
+			redirect_to articles_path
+		else
+			render 'new'
+		end
+	end
+
+
+	private
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
+	end
+
+
 end
