0de87ad0a26fb3a42884ad166722ca0fc86c3a41
Iegor <vlasenkoiegor2021@gmail.com>
2021-05-28 12:19:45 +1000

    authentication feature added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_002500) do
+ActiveRecord::Schema.define(version: 2021_05_27_013030) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_05_27_002500) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
