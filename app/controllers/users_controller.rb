5553c1cd5208989496977c68715f21e233bb0b9d
Iegor <vlasenkoiegor2021@gmail.com>
2021-06-30 14:17:00 +1000

    admin functionality added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:show, :edit, :update, :destroy]
 	before_action :require_user, only: [:edit, :update]
-	# before_action :require_same_user, only: [:edit, :update, :destroy]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def show		
 		@articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
 
 	def destroy
 		@user.destroy
-		session[:user_id] = nil
+		session[:user_id] = nil if @user == current_user
 		flash[:notice] = "Account and all associated articles deleted"
 		redirect_to articles_path
 	end
@@ -53,6 +53,13 @@ class UsersController < ApplicationController
 
 	def set_user
 		@user = User.find(params[:id])
+	end
+
+	def require_same_user
+		if current_user != @user && !current_user.admin?
+			flash[:alert] = "You can only edit or delete your own article"
+			redirect_to @article
+		end
 	end
 
 
