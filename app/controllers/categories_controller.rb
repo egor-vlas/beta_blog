1e6a45b1ea4ed3053db1428df1ab4ef3b0b5e7ee
Iegor <vlasenkoiegor2021@gmail.com>
2021-07-02 09:03:08 +1000

    Categories controller created

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,13 @@
+class CategoriesController < ApplicationController
+	def new
+
+	end
+
+	def index
+
+	end
+
+	def show
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'  
+  delete 'logout', to: 'sessions#destroy'
+
+	resources :categories, except: [:destroy]
 
 end
 
