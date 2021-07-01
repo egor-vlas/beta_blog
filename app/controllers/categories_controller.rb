6c77df0fc61a955d7b8655b98eb4abdddc84627c
Iegor <vlasenkoiegor2021@gmail.com>
2021-07-02 09:44:51 +1000

    Create category added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
 	def new
-
+		@category = Category.new
 	end
 
 	def index
@@ -9,5 +9,20 @@ class CategoriesController < ApplicationController
 
 	def show
 
+	end
+
+	def create
+		@category = Category.new(category_params)
+		if @category.save
+			flash[:notice] = "Category was successfully created"
+			redirect_to @category
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def category_params
+		params.require(:category).permit(:name)
 	end
 end
