46afb7bb7e539ce76508501b15b733b7de7a9df5
Iegor <vlasenkoiegor2021@gmail.com>
2021-07-03 10:01:13 +1000

    admin requirements added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,7 @@
 class CategoriesController < ApplicationController
+	before_action :require_admin, except: [:index, :show]
+
+
 	def new
 		@category = Category.new
 	end
@@ -25,4 +28,13 @@ class CategoriesController < ApplicationController
 	def category_params
 		params.require(:category).permit(:name)
 	end
+
+	def require_admin
+		if !(logged_in? && current_user.admin?)
+			flash[:alert] = "Only admins can perform that action"
+			redirect_to categories_path
+		end
+	end
+
+
 end
